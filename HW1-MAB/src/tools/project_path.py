1044bb263344a47135f46cf02457129a1d6fbe19
Zihan WANG <zihan.wang@magnumwm.com>
2022-03-06 12:20:55 +0800

    upload data

--- HW1-MAB/src/tools/project_path.py
+++ HW1-MAB/src/tools/project_path.py
@@ -2,6 +2,5 @@ from os.path import dirname, abspath, join
 
 root_path = dirname(dirname(dirname(abspath(__file__))))
 data_path = join(root_path, 'data')
-price_volume_path = join(data_path, 'price_volume')
 cleaned_data_path = join(data_path, 'cleaned_data')
 # %%
